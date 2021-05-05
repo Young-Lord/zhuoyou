f410576ecf15cbe70cdfac52d38a37bdde159ac1
Young-Lord <2926182876@qq.com>
2021-05-05 12:19:16 +0800

    [格式化]

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import sys
 
 BLACK = [0, 0, 0]
 GREY = [127, 127, 127]
-YELLOW=[255,255,0]
-BLUE=[0,0,255]
+YELLOW = [255, 255, 0]
+BLUE = [0, 0, 255]
 FIGMA = [196, 196, 196]  # 0xc4
 
 pygame.init()
@@ -17,23 +17,23 @@ def rect(color, place):
 
 
 rect(FIGMA, (358, 0, 724, 89))
-rect(FIGMA,(0,188,89,647))
-rect(FIGMA,(1351,188,89,647))
-#其他玩家
-rect(YELLOW,(309,289,822,446))
-#地图
-rect((0xb1,0x9d,0x9d),(1037,753,188,72))
-#结束按钮
-rect((0,128,0),(0,864,864,160))
-#背包
-for i in range(864,984+1,40):
-    rect([k+(i-864)*20//40 for k in GREY],(864,i,208,40))
-#装备
-for i in range(864,944+1,80):
-    rect([k+(i-864)*20//80 for k in GREY],(1072,i,208,80))
-#技能
-rect(BLUE,(1280,864,160,160))
-#角色头像
+rect(FIGMA, (0, 188, 89, 647))
+rect(FIGMA, (1351, 188, 89, 647))
+# 其他玩家
+rect(YELLOW, (309, 289, 822, 446))
+# 地图
+rect((0xb1, 0x9d, 0x9d), (1037, 753, 188, 72))
+# 结束按钮
+rect((0, 128, 0), (0, 864, 864, 160))
+# 背包
+for i in range(864, 984+1, 40):
+    rect([k+(i-864)*20//40 for k in GREY], (864, i, 208, 40))
+# 装备
+for i in range(864, 944+1, 80):
+    rect([k+(i-864)*20//80 for k in GREY], (1072, i, 208, 80))
+# 技能
+rect(BLUE, (1280, 864, 160, 160))
+# 角色头像
 
 pygame.display.flip()
 while True:
