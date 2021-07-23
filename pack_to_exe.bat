e5cfad1fedb8d398f854868bb787975829546575
Young-Lord <2926182876@qq.com>
2021-07-23 15:50:33 +0800

    试图修复自动打包脚本

--- pack_to_exe.bat
+++ pack_to_exe.bat
@@ -2,9 +2,9 @@ cd src
 python main.py --genonly
 set packer=packer_tmp.py
 copy ..\%packer% .
-pyinstaller %packer% -F --distpath src --clean -n game.exe
+pyinstaller %packer% -F --distpath . --clean -n game.exe
 REM pyinstaller %packer% -w -F --distpath src --clean -n game.exe
 del %packer%
 rmdir build /S /Q
 rmdir __pycache__ /S /Q
-REM del game.exe.spec
+del game.exe.spec
